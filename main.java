e4143ff968c80350b2567ea7b3ebd50c3ad09acb
Ornab05 <ornabpurkayastha@gmail.com>
2024-01-25 01:14:53 -0500

    using the while loop

--- main.java
+++ main.java
@@ -1,5 +1,19 @@
-public class Main {
+public class main{
     public static void main(String[] args) {
       System.out.println("Hello World");
+      System.out.println("How are you doing");
+      System.out.println("How are you doing");
+      int number= 5+10;
+      System.out.println(number);
+
+
+
+      // using the while loop
+
+      int i = 0;
+      while(i<=5){
+        System.out.println(i);
+        i++;
+      }
     }
   }
