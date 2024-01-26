b5d417b94a29f11f1e582e5b6c78e728bed59b19
Ornab05 <ornabpurkayastha@gmail.com>
2024-01-26 14:02:55 -0500

    encapsulization

--- oopProblemSolve.java
+++ oopProblemSolve.java
@@ -1,3 +1,33 @@
+// class created for encapsulization problem solving
+class Rectangle {
+
+    
+    private int length;
+    private int width;
+  
+
+    public Rectangle() {
+      this.length = 0;
+      this.width = 0;
+    }
+  
+    
+    public Rectangle(int length, int width) {
+      this.length = length;
+      this.width = width;
+  
+    }
+  
+    
+    public int getArea() {
+      return this.length * this.width; 
+    }
+}
+
+
+
+// the main class
+
 public class oopProblemSolve {
 
     public double squareSum(double num1, double num2, double num3) {
@@ -11,6 +41,7 @@ public class oopProblemSolve {
     
         return sum;
       }
+   
 
 
     public static void main(String[] args) {
@@ -19,5 +50,15 @@ public class oopProblemSolve {
 
         System.out.println(s.squareSum(5, 6, 7));
 
+
+        // encapsulization problem solving
+
+        Rectangle any= new Rectangle(5,7);
+        System.out.println(any.getArea());
+
+
+       
     }
 }
+
+
